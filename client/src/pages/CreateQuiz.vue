a598abc712a726cd4f942fb31d9082b9d76964e0
Anton Gunnarsson <gson.anton@hotmail.com>
2017-11-05 17:01:21 +0100

    Improve CreateQuiz page

--- client/src/pages/CreateQuiz.vue
+++ client/src/pages/CreateQuiz.vue
@@ -2,8 +2,14 @@
   .create-quiz
     h1.title.has-text-centered Create Quiz
     step-bar
-    a.button.is-info Log in with Spotify
-    a Why?
+    a.button.is-dark.is-medium
+      span Log in with Spotify
+      span.icon
+        i.fa.fa-spotify
+    .why-spotify
+      h2 Why do I have to log in?
+      p Det är ett välkänt faktum att läsare distraheras av läsbar text på en sida när man skall studera layouten. Poängen med Lorem Ipsum är att det ger ett normalt ordflöde, till skillnad från "Text här, Text här", och ger intryck av att vara läsbar text. Många publiseringprogram och webbutvecklare använder Lorem Ipsum som test-text.
+    router-link.back-button(:to="{ name: 'landing' }") No thanks
 </template>
 
 <script>
@@ -31,7 +37,6 @@ export default {
   color: white;
 
   margin-top: auto;
-  font-weight: bold;
 
   h1 {
     font-family: 'Pacifico', cursive;
@@ -43,15 +48,29 @@ export default {
     margin-bottom: 2.5rem;
   }
 
-  h2 {
-    color: white;
-    font-weight: normal;
-    text-shadow: 0 1px 2px rgba(0, 0, 0, 0.20);
-    font-size: 2rem;
-  }
-
   .button {
     margin: 1rem 0;
+    font-weight: bold;
+  }
+
+  .why-spotify {
+    h2 {
+      font-weight: bold;
+    }
+
+    max-width: 500px;
+    padding: 0 30px;
+  }
+
+  .back-button {
+    margin-top: 1rem;
+
+    color: rgba(255, 255, 255, 0.7);
+    text-decoration: underline;
+
+    &:hover {
+      color: white;
+    }
   }
 }
 </style>
