24c1f6f2e7033342ba80f5a13c7d8624f0cd20b3
Anton Gunnarsson <gson.anton@hotmail.com>
2017-11-13 00:14:41 +0100

    Add player view for create quiz

--- client/src/components/CreateQuizPlayers.vue
+++ client/src/components/CreateQuizPlayers.vue
@@ -1,13 +1,52 @@
 <template lang="pug">
-
+  .create-quiz--players
+    .pin
+      span Give your friends this PIN:
+      horizontal-line-heading.pin ABC123
+    .players
+      player-list
+    .settings
+      h3 Selected playlist
+      playlist-list-item(:playlist="selectedPlaylist")
 </template>
 
 <script>
-export default {
+import HorizontalLineHeading from '@/components/HorizontalLineHeading'
+import PlaylistListItem from '@/components/PlaylistListItem'
+import PlayerList from '@/components/PlayerList'
 
+export default {
+  components: {
+    PlaylistListItem,
+    HorizontalLineHeading,
+    PlayerList
+  },
+  computed: {
+    selectedPlaylist () {
+      return this.$store.state.selectedPlaylist
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.create-quiz--players {
+  .pin {
+    text-align: center;
 
+    h2 {
+      font-size: 2rem;
+      font-weight: bold;
+    }
+  }
+
+  .settings {
+    margin-top: 1rem;
+
+    h3 {
+      margin: 0.2rem 0;
+      font-weight: bold;
+    }
+  }
+}
 </style>
