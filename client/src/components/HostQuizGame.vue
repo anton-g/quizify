3f0ab7d2ca70371220a0351b102225f1fcf2a228
Anton Gunnarsson <gson.anton@hotmail.com>
2017-12-17 00:35:52 +0100

    Add very basic game in progress view

---
+++ client/src/components/HostQuizGame.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .host-game
+    h1.title {{ artist + ' - ' + title }}
+    img(:src="image")
+    h1.title Vad heter artisten?
+</template>
+
+<script>
+export default {
+  created () {
+    this.$store.dispatch('playNextTrack')
+
+    setTimeout(() => {
+      this.$store.dispatch('pause')
+    }, 2500)
+
+    setTimeout(() => {
+      this.$store.dispatch('resume')
+    }, 6000)
+  },
+  computed: {
+    track () {
+      return this.$store.state.tracks[0]
+    },
+    image () {
+      return this.track.album.images[0].url
+    },
+    artist () {
+      return this.track.artists[0].name
+    },
+    title () {
+      return this.track.name
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.host-game h1 {
+  color: white !important;
+  text-align: center;
+}
+</style>
