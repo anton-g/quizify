1dad5b2ea474869884cf311d3e49a0cbe98af1bd
Anton Gunnarsson <gson.anton@hotmail.com>
2017-12-22 19:41:55 +0100

    Add ModalBase

---
+++ client/src/components/ModalBase.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .modal(v-bind:class="{ 'is-active': this.active }")
+    .modal-background(@click="$emit('close')")
+      slot(name="background")
+    .modal-content
+      slot
+    .modal-close(v-if="!hideCloseButton", @click="$emit('close')")
+</template>
+
+<script>
+export default {
+  name: 'modalbase',
+  props: {
+    active: {
+      type: Boolean,
+      required: true
+    },
+    hideCloseButton: {
+      type: Boolean,
+      default: () => {
+        return false
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
