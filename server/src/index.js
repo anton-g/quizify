4028569b5aa14d0099bbf936823377750cb81a9a
Anton Gunnarsson <gson.anton@hotmail.com>
2018-03-30 16:49:03 +0200

    Remove old server

--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http').createServer()
-const io = require('socket.io')(http)
-const Server = require('./Server')
-
-const server = new Server(io)
-console.log(!!(server))
-const port = process.env.PORT || 8081
-http.listen(port, () => { console.log(`listening on *:${port}`) })
