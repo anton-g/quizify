2a55c22916ffaecfd36d0241ec716e76fb102b35
Anton Gunnarsson <gson.anton@hotmail.com>
2018-04-01 21:51:27 +0200

    Setup naive cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, NestApplication } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { NestApplicationOptions } from '@nestjs/common/interfaces/nest-application-options.interface';
+import * as cors from 'cors';
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
+  const app = await NestFactory.create(ApplicationModule);
+  app.use(cors());
 	await app.listen(3000);
 }
 bootstrap();
