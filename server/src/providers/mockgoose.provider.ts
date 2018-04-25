fd791e41688180054951c1cf555f408631017f6c
Anton Gunnarsson <gson.anton@hotmail.com>
2018-04-25 22:18:37 +0200

    Add better parallel test workaround

--- server/src/providers/mockgoose.provider.ts
+++ server/src/providers/mockgoose.provider.ts
@@ -5,6 +5,26 @@ export const mockgooseProvider = {
   provide: 'MockgooseToken',
   useFactory: async () => {
     (mongoose as any).Promise = global.Promise;
+
+    // fix parallel tests, https://github.com/Mockgoose/Mockgoose/issues/22
+    Mockgoose.prototype.prepareStorage = function() {
+      const _this = this;
+      return new Promise(function(resolve, reject) {
+        Promise.all([_this.getTempDBPath(), _this.getOpenPort()]).then(promiseValues => {
+          const dbPath = promiseValues[0];
+          const openPort = promiseValues[1].toString();
+          const storageEngine = _this.getMemoryStorageName();
+          const mongodArgs = ['--port', openPort, '--storageEngine', storageEngine, '--dbpath', dbPath];
+          _this.mongodHelper.mongoBin.commandArguments = mongodArgs;
+          const mockConnection = () => {
+            _this.mockConnectCalls(_this.getMockConnectionString(openPort));
+            resolve();
+          };
+          _this.mongodHelper.run().then(mockConnection).catch(mockConnection);
+        });
+      });
+    };
+
     const mockgoose = new Mockgoose(mongoose);
     mockgoose.prepareStorage().then(async () => {
       await mongoose.connect('mongodb://localhost:3000/quizify-test');
