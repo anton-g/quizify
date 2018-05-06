ac98626c27d3967e0697dbcf416f85cce9a9d7bd
Anton Gunnarsson <gson.anton@hotmail.com>
2018-05-06 20:11:12 +0200

    Refactor into Home view

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,36 +3,8 @@
     .header
       h1.title Quizify
     .content
-      .join
-        h2 Join quiz
-        .field
-          .control.has-icons-left
-            input.input(type="text", placeholder="key")
-            span.icon.is-small.is-left
-              FontAwesomeIcon(:icon="icon")
-        .field
-          .control
-            button.button.is-dark.is-fullwidth Join
-      .create
-        h2 Create quiz
+      router-view
 </template>
-
-<script>
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import { faKey } from '@fortawesome/fontawesome-free-solid'
-
-export default {
-  components: {
-    FontAwesomeIcon,
-  },
-  computed: {
-    icon () {
-      return faKey
-    }
-  }
-}
-</script>
-
 
 <style lang="sass" src="bulma"></style>
 <style lang="scss">
@@ -55,40 +27,6 @@ html, body {
   .content {
     grid-column: 2;
     grid-row: 2;
-
-    h2 {
-      font-weight: 900;
-    }
-
-    .join, .create {
-      margin-bottom: 24px;
-      background-color: white;
-      border-radius: 8px;
-      box-shadow: 0px 2px 4px rgba($color: #000000, $alpha: 0.18);
-      overflow: hidden;
-
-      input {
-        border: none;
-        width: 100%;
-        font-size: 16px;
-        padding: 8px 32px;
-        background-color: hsl(210, 9%, 96%);
-        border-radius: 0;
-
-        &::placeholder {
-          color: hsl(210, 9%, 66%);
-        }
-      }
-
-      button {
-        border: none;
-        border-radius: 0;
-      }
-
-      h2 {
-        padding: 16px 0 0 16px;
-      }
-    }
   }
 
   .header {
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,71 @@
 <template lang="pug">
   .home
-    h1 Quizify
+    .join
+      h2 Join quiz
+      .field
+        .control.has-icons-left
+          input.input(type="text", placeholder="Input quiz key", maxlength="6")
+          span.icon.is-small.is-left
+            FontAwesomeIcon(:icon="keyIcon")
+      .field
+        .control
+          button.button.is-dark.is-fullwidth Join
+    .create
+      h2 Create quiz
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { faKey } from '@fortawesome/fontawesome-free-solid'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    FontAwesomeIcon,
   },
+  computed: {
+    keyIcon () {
+      return faKey
+    }
+  }
 };
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  font-weight: 900;
+}
+
+.join,
+.create {
+  margin-bottom: 24px;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba($color: #000000, $alpha: 0.18);
+  overflow: hidden;
+
+  input {
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    padding: 8px 32px;
+    background-color: hsl(210, 9%, 96%);
+    border-radius: 0;
+    text-transform: uppercase;
+
+    &::placeholder {
+      color: hsl(210, 9%, 66%);
+      text-transform: none;
+    }
+  }
+
+  button {
+    border: none;
+    border-radius: 0;
+  }
+
+  h2 {
+    padding: 16px 0 0 16px;
+  }
+}
+</style>
