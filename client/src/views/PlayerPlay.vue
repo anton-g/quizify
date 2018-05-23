e1183870432cde9343a72aa86a0a855047dc3a86
Anton Gunnarsson <gson.anton@hotmail.com>
2018-05-23 20:25:39 +0200

    Redesign buzzer button

--- client/src/views/PlayerPlay.vue
+++ client/src/views/PlayerPlay.vue
@@ -2,7 +2,8 @@
   .play
     card.buzzer
       h2.title Buzzer
-      button.button.is-fullwidth.is-danger(@click="buzz", :disabled="paused")
+      a(@click="buzz", :class="{ disabled: paused }")
+        span Buzz!
     card.stats
       h2.title Stats
       p Score: {{ playerScore }}
@@ -43,13 +44,63 @@ export default {
   min-height: 100%;
 
   .buzzer {
-    margin-bottom: $size-2;
-    display: flex;
-    flex-direction: column;
+    margin-bottom: auto;
 
-    button {
-      flex: 1;
-      border-radius: $size-2;
+    a {
+      cursor: pointer;
+      user-select: none;
+      display: inline-block;
+      border-radius: 8px;
+      box-shadow: 0 8px 0 #c53737, 0 15px 20px rgba(0, 0, 0, .35);
+      transition: box-shadow .05s ease-in-out;
+      transition: filter .1s ease-in-out;
+      font-size: 60px;
+      color: #fff;
+      width: 100%;
+      text-align: center;
+      margin-bottom: $size-2;
+
+      span {
+        width: 100%;
+        display: inline-block;
+        padding: 100px 30px;
+        background-color: #ec5252;
+        background-image: linear-gradient(rgba(249, 158, 158, 0.8), rgba(247, 109, 109, 0.2));
+        border-radius: 8px;
+        box-shadow: inset 0 -1px 1px rgba(255, 255, 255, .15);
+        font-family: 'Pacifico', Arial, sans-serif;
+        line-height: 1;
+        text-shadow: 0 -1px 1px rgba(175, 49, 95, .7);
+        transition: background-color .2s ease-in-out, transform .1s ease-in-out;
+      }
+
+      &:hover span {
+        background-color: #ec6a6a;
+        text-shadow: 0 -1px 1px rgba(175, 49, 49, 0.9), 0 0 5px rgba(255, 255, 255, .8);
+      }
+
+      &:active, &:focus {
+        box-shadow: 0 8px 0 #c53737, 0 12px 10px rgba(0, 0, 0, .3);
+      }
+
+      &:active span {
+        transform: translate(0, 6px);
+      }
+
+      &.disabled {
+        box-shadow: 0 8px 0 #c53737, 0 12px 10px rgba(0, 0, 0, .3);
+        cursor: not-allowed;
+        filter: opacity(70%);
+
+        span {
+          transform: translate(0, 6px);
+        }
+
+        &:hover span {
+          background-color: #ec5252;
+          text-shadow: 0 -1px 1px rgba(175, 49, 95, .7);;
+        }
+      }
     }
   }
 
