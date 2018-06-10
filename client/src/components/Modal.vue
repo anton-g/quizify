564216fc7fd84273f15b2e96ad73b0e584bbf991
Anton Gunnarsson <gson.anton@hotmail.com>
2018-06-10 18:31:51 +0200

    Add modal component

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .modal
+    .modal-background
+    .modal-content
+      slot(name="content")
+        card
+          slot
+    slot(name="close")
+      button.modal-close.is-large(aria-label="close", @click="$emit('close')")
+</template>
+
+<script>
+import Card from './Card.vue'
+
+export default {
+  components: {
+    Card
+  },
+  name: 'modal'
+}
+</script>
