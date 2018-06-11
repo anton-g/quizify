8153b7f26ebd056a84d9dc4fc6e5ad4cbe4bc080
Anton Gunnarsson <gson.anton@hotmail.com>
2018-06-11 20:57:35 +0200

    Add user playlist design to playlist selection

--- client/src/views/HostCreate.vue
+++ client/src/views/HostCreate.vue
@@ -11,20 +11,75 @@
         button.button.is-dark.is-fullwidth Create
     modal(:active="showModal", @close="showModal = false")
       h2.title Featured playlists
-      .featured-playlists
-        a.featured-playlist(:style="{ background: '#a4b6dd' }")
-          img(src="https://mosaic.scdn.co/640/11b1054a8d4b86106085ec30073e50e6f584639e6be8b8cd5690385c28304aafc58ace5ce6dbdc977c6ab0bd116e14ecdd58299a1d103df4e195ac2d93e156ad7762357bea213e53cf346e5f8fe3efae")
-          span.name Alla ska med
-          span.info 23 tracks
-        a.featured-playlist(:style="{ background: '#d09292' }")
-          img(src="https://i.scdn.co/image/8cb5ccc8a642e06a69dbdb2f0c47d597057cb3b1")
-          span.name Dansband gör covers
-          span.info 54 tracks
-        a.featured-playlist(:style="{ background: '#c094cc' }")
-          img(src="https://mosaic.scdn.co/640/2993cba1f3c5e99613b7c5d1cc7df07e7d71cb8679970c9df607f89a5895e9b1aac1f4d1b5281d197c99322cf4f696082b86d92a5e4ab93e6cc49bfafa75bec32c71f5f4339c41b6ca816f4dd4cac111")
-          span.name Generationsquiz
-          span.info 301 tracks
-      h2.title Your playlists
+      .featured-playlists-wrapper
+        .featured-playlists
+          a.featured-playlist(:style="{ background: '#a4b6dd' }")
+            img(src="https://mosaic.scdn.co/640/11b1054a8d4b86106085ec30073e50e6f584639e6be8b8cd5690385c28304aafc58ace5ce6dbdc977c6ab0bd116e14ecdd58299a1d103df4e195ac2d93e156ad7762357bea213e53cf346e5f8fe3efae")
+            span.name Alla ska med
+            span.info 23 tracks
+          a.featured-playlist(:style="{ background: '#d09292' }")
+            img(src="https://i.scdn.co/image/8cb5ccc8a642e06a69dbdb2f0c47d597057cb3b1")
+            span.name Dansband gör covers
+            span.info 54 tracks
+          a.featured-playlist(:style="{ background: '#c094cc' }")
+            img(src="https://mosaic.scdn.co/640/2993cba1f3c5e99613b7c5d1cc7df07e7d71cb8679970c9df607f89a5895e9b1aac1f4d1b5281d197c99322cf4f696082b86d92a5e4ab93e6cc49bfafa75bec32c71f5f4339c41b6ca816f4dd4cac111")
+            span.name Generationsquiz
+            span.info 301 tracks
+        a.more-link.button.is-text See more
+      .user-playlists-wrapper
+        h2.title Your playlists
+        .user-playlists
+          ul
+            li.user-playlist
+              a
+                span.name Julquiz
+                span.info 10 tracks
+            li.user-playlist
+              a
+                span.name Amanda 25
+                span.info 18 tracks
+            li.user-playlist
+              a
+                span.name Lajvet
+                span.info 22 tracks
+            li.user-playlist
+              a
+                span.name Always bushes of seagulls
+                span.info 10 tracks
+            li.user-playlist
+              a
+                span.name sümmer
+                span.info 199 tracks
+            li.user-playlist
+              a
+                span.name Och vi ska också glömmas bort
+                span.info 30 tracks
+            li.user-playlist
+              a
+                span.name Julquiz
+                span.info 10 tracks
+            li.user-playlist
+              a
+                span.name Amanda 25
+                span.info 18 tracks
+            li.user-playlist
+              a
+                span.name Lajvet
+                span.info 22 tracks
+            li.user-playlist
+              a
+                span.name Always bushes of seagulls
+                span.info 10 tracks
+            li.user-playlist
+              a
+                span.name sümmer
+                span.info 199 tracks
+            li.user-playlist
+              a
+                span.name Och vi ska också glömmas bort
+                span.info 30 tracks
+        a.more-link.button.is-text See more
+
 </template>
 
 <script>
@@ -53,44 +108,100 @@ export default {
 <style lang="scss" scoped>
 @import '~@design';
 
-.featured-playlists {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
-  column-gap: $size-2;
+.featured-playlists-wrapper {
+  display: flex;
+  flex-direction: column;
 
-  .featured-playlist {
-    height: 190px;
-    width: 190px;
-    border-radius: $size-2;
+  .featured-playlists {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+    column-gap: $size-2;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    position: relative;
-    color: white;
-    overflow: hidden;
-
-    img {
-      position: absolute;
-      top: 0;
-      width: 190px;
+    .featured-playlist {
       height: 190px;
-      z-index: 1;
-      mix-blend-mode: multiply;
-      filter: grayscale(100%);
-    }
+      width: 190px;
+      border-radius: $size-2;
+      box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.18);
 
-    .name {
-      padding-left: $size-1;
-      font-weight: bold;
-      z-index: 2;
-    }
+      transition: box-shadow 0.3s, transform 0.3s;
 
-    .info {
-      padding-left: $size-1;
-      margin-bottom: $size-1;
-      z-index: 2;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      position: relative;
+      color: white;
+      overflow: hidden;
+
+      img {
+        position: absolute;
+        top: 0;
+        width: 190px;
+        height: 190px;
+        z-index: 1;
+        mix-blend-mode: multiply;
+        filter: grayscale(100%);
+      }
+
+      .name {
+        padding-left: $size-1;
+        font-weight: bold;
+        z-index: 2;
+        text-shadow: 1px 1px rgba(0,0,0,0.15);
+      }
+
+      .info {
+        padding-left: $size-1;
+        margin-bottom: $size-1;
+        z-index: 2;
+        text-shadow: 1px 1px rgba(0,0,0,0.15);
+      }
+
+      &:hover {
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.24);
+        transform: translate(0px, -4px);
+      }
     }
   }
+}
+
+@media screen and (max-width: 900px) {
+  .featured-playlists {
+    grid-auto-flow: row;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(auto-fit, minmax(30%, 1fr));
+  }
+}
+
+.user-playlists-wrapper {
+  display: flex;
+  flex-direction: column;
+
+  .user-playlists {
+    .user-playlist {
+      border-radius: $size-1;
+
+      a {
+        display: flex;
+        justify-content: space-between;
+        padding: $size-1;
+
+        .name {
+          color: black;
+        }
+
+        .info {
+          color: rgba(0, 0, 0, 0.5);
+        }
+      }
+
+      &:hover {
+        background-color: whitesmoke;
+      }
+    }
+  }
+}
+
+.more-link {
+  margin: $size-2 0 0 auto;
 }
 </style>
