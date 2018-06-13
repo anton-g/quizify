208b86e75f60f9542d87e69bcfe927667cfc0318
Anton Gunnarsson <gson.anton@hotmail.com>
2018-06-13 22:38:03 +0200

    Add playlist info/button to host lobby

--- client/src/store/modules/host.js
+++ client/src/store/modules/host.js
@@ -8,7 +8,50 @@ const socketBus = new Vue()
 
 const state = {
   playlist: undefined,
-  quiz: undefined
+  quiz: undefined,
+  featuredPlaylists: [
+    {
+      name: 'Alla ska med',
+      length: 23,
+      img: 'https://mosaic.scdn.co/640/11b1054a8d4b86106085ec30073e50e6f584639e6be8b8cd5690385c28304aafc58ace5ce6dbdc977c6ab0bd116e14ecdd58299a1d103df4e195ac2d93e156ad7762357bea213e53cf346e5f8fe3efae'
+    },
+    {
+      name: 'Dansband gör covers',
+      length: 54,
+      img: 'https://i.scdn.co/image/8cb5ccc8a642e06a69dbdb2f0c47d597057cb3b1'
+    },
+    {
+      name: 'Generationsquiz',
+      length: 301,
+      img: 'https://mosaic.scdn.co/640/2993cba1f3c5e99613b7c5d1cc7df07e7d71cb8679970c9df607f89a5895e9b1aac1f4d1b5281d197c99322cf4f696082b86d92a5e4ab93e6cc49bfafa75bec32c71f5f4339c41b6ca816f4dd4cac111'
+    }
+  ],
+  playlists: [
+    {
+      name: 'Julquiz',
+      length: 10
+    },
+    {
+      name: 'Amanda 25',
+      length: 18
+    },
+    {
+      name: 'Lajvet',
+      length: 22
+    },
+    {
+      name: 'Always bushes of seagulls',
+      length: 10
+    },
+    {
+      name: 'Sümmer',
+      length: 199
+    },
+    {
+      name: 'Och vi ska också glömmas bort',
+      length: 98
+    }
+  ]
 }
 
 const getters = {
--- client/src/views/HostLobby.vue
+++ client/src/views/HostLobby.vue
@@ -1,19 +1,60 @@
 <template lang="pug">
   card.lobby
     h2.title Lobby
+    ImageButton(
+      :img="selectedPlaylist.img",
+      :text="selectedPlaylist.name",
+      subtext="Click to change"
+      height="70px",
+      width="100%",
+      color="#52494c",
+      @click="showPlaylistPicker = true"
+    )
     button.button Start
     h3.title.is-4 Players
     player-list
+    playlist-picker(
+      :active="showPlaylistPicker",
+      :featuredPlaylists="featuredPlaylists",
+      :playlists="userPlaylists"
+      @close="showPlaylistPicker = false",
+      @select="select")
 </template>
 
 <script>
 import Card from '../components/Card.vue'
 import PlayerList from '../components/PlayerList.vue'
+import ImageButton from '../components/ImageButton.vue'
+import PlaylistPicker from '../components/PlaylistPicker.vue'
 
 export default {
   components: {
     Card,
-    PlayerList
+    PlayerList,
+    ImageButton,
+    PlaylistPicker
+  },
+  data () {
+    return {
+      showPlaylistPicker: false
+    }
+  },
+  computed: {
+    featuredPlaylists () {
+      return this.$store.state.host.featuredPlaylists.slice(0, 3)
+    },
+    userPlaylists () {
+      return this.$store.state.host.playlists
+    },
+    selectedPlaylist () {
+      return this.$store.state.host.playlist
+    }
+  },
+  methods: {
+    select (playlist) {
+      this.$store.dispatch('selectPlaylist', playlist)
+      this.showPlaylistPicker = false
+    }
   }
 }
 </script>
