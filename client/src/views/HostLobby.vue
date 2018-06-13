8223b27a7b24df20e054b0d8a5a9d99060e4b95f
Anton Gunnarsson <gson.anton@hotmail.com>
2018-06-13 21:24:48 +0200

    Add player list to HostLobby

--- client/src/views/HostLobby.vue
+++ client/src/views/HostLobby.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
   card.lobby
     h2.title Lobby
+    button.button Start
+    h3.title.is-4 Players
+    player-list
 </template>
 
 <script>
 import Card from '../components/Card.vue'
+import PlayerList from '../components/PlayerList.vue'
 
 export default {
   components: {
-    Card
+    Card,
+    PlayerList
   }
 }
 </script>
