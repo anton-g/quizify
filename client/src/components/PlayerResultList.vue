709ca8f959656366074b661fe4f649604a09453e
Anton Gunnarsson <gson.anton@hotmail.com>
2018-07-26 15:04:40 +0200

    Refactor player results list to new component

---
+++ client/src/components/PlayerResultList.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+  .player-result-list
+    ul.result-list
+      li.player(v-for="(player, idx) in results")
+        .fill(:style="{ width: fillWidth(player) + '%' }")
+        .icon-wrapper(:style="{ visibility: idx > 0 ? 'hidden' : 'visible' }")
+          FontAwesomeIcon(:icon="trophyIcon")
+        .name {{ player.name }}
+        .score {{ player.score }}
+          span pts
+</template>
+
+<script>
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { faTrophy } from '@fortawesome/fontawesome-free-solid'
+
+export default {
+  name: 'playerresultlist',
+  components: {
+    FontAwesomeIcon
+  },
+  props: ['results'],
+  computed: {
+    maxScore () {
+      return this.results[0].score
+    },
+    trophyIcon () {
+      return faTrophy
+    }
+  },
+  methods: {
+    fillWidth (player) {
+      return (player.score / this.maxScore) * 100
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@design';
+
+.player {
+  display: flex;
+  background-color: rgba(235, 235, 235, 1.0);
+  border-radius: $size-1;
+  position: relative;
+  overflow: hidden;
+
+  &:not(:last-child) {
+    margin-bottom: $size-1;
+  }
+
+  .fill {
+    position: absolute;
+    height: 100%;
+    background-color: #42e695;
+    z-index: 1;
+    outline: 4px solid white;
+  }
+
+  .name {
+    padding: $size-1;
+    z-index: 2;
+    font-weight: bold;
+  }
+
+  .score {
+    margin-left: auto;
+    padding: 0 $size-2;
+    z-index: 2;
+    font-size: $size-3;
+
+    span {
+      margin-left: 2px;
+      font-size: $size-2;
+      color: rgba(0, 0, 0, 0.5)
+    }
+  }
+
+  .icon-wrapper {
+    padding: $size-1 0 $size-1 $size-1;
+    z-index: 2;
+  }
+}
+</style>
--- client/src/views/PlayerEnd.vue
+++ client/src/views/PlayerEnd.vue
@@ -1,14 +1,7 @@
 <template lang="pug">
   card.results
     h2.title {{ title }}
-    ul.result-list
-      li.player(v-for="(player, idx) in results")
-        .fill(:style="{ width: fillWidth(player) + '%' }")
-        .icon-wrapper(:style="{ visibility: idx > 0 ? 'hidden' : 'visible' }")
-          FontAwesomeIcon(:icon="trophyIcon")
-        .name {{ player.name }}
-        .score {{ player.score }}
-          span pts
+    player-result-list.result-list(:results="results")
     .field
       .control
         button.button.is-dark.is-pulled-right(@click="home") Home
@@ -16,14 +9,13 @@
 
 <script>
 import Card from '@/components/Card.vue'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import { faTrophy } from '@fortawesome/fontawesome-free-solid'
+import PlayerResultList from '@/components/PlayerResultList.vue'
 
 export default {
   name: 'playerend',
   components: {
     Card,
-    FontAwesomeIcon
+    PlayerResultList
   },
   computed: {
     results () {
@@ -34,18 +26,9 @@ export default {
     },
     title () {
       return this.won ? 'You won!' : 'Game ended'
-    },
-    maxScore () {
-      return this.results[0].score
-    },
-    trophyIcon () {
-      return faTrophy
     }
   },
   methods: {
-    fillWidth (player) {
-      return (player.score / this.maxScore) * 100
-    },
     home () {
       this.$router.push({ name: 'home' })
       this.$store.dispatch('cleanup')
@@ -57,53 +40,7 @@ export default {
 <style lang="scss" scoped>
 @import '~@design';
 
-.results {
-  .result-list {
-    margin-bottom: $size-2;
-
-    .player {
-      display: flex;
-      background-color: rgba(235, 235, 235, 1.0);
-      border-radius: $size-1;
-      position: relative;
-      overflow: hidden;
-
-      &:not(:last-child) {
-        margin-bottom: $size-1;
-      }
-
-      .fill {
-        position: absolute;
-        height: 100%;
-        background-color: #42e695;
-        z-index: 1;
-        outline: 4px solid white;
-      }
-
-      .name {
-        padding: $size-1;
-        z-index: 2;
-        font-weight: bold;
-      }
-
-      .score {
-        margin-left: auto;
-        padding: 0 $size-2;
-        z-index: 2;
-        font-size: $size-3;
-
-        span {
-          margin-left: 2px;
-          font-size: $size-2;
-          color: rgba(0, 0, 0, 0.5)
-        }
-      }
-
-      .icon-wrapper {
-        padding: $size-1 0 $size-1 $size-1;
-        z-index: 2;
-      }
-    }
-  }
+.result-list {
+  margin-bottom: $size-2;
 }
 </style>
