12dd92a744ed7fc730bf2cb50d02c3c206ad779d
Anton Gunnarsson <gson.anton@hotmail.com>
2018-08-04 14:05:06 +0200

    Add socket auth guard

---
+++ server/src/common/EventAuth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/services/user.service';
+import * as jwt from 'jsonwebtoken';
+import { ConfigService } from '../config/config.service';
+
+@Injectable()
+export class EventAuthGuard implements CanActivate {
+  constructor (private readonly config: ConfigService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const [ data ] = context.switchToWs().getData()
+    const token = data.authorization
+
+    if (!token) {
+      return false
+    }
+
+    try {
+      const payload = jwt.verify(token, this.config.jwtSecret)
+      return true
+    } catch (err) {
+      return false
+    }
+  }
+}
