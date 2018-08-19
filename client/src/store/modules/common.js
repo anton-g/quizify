7b3ede886b7ff5612381403046e62fdcc5c446b7
Anton Gunnarsson <gson.anton@hotmail.com>
2018-08-19 16:54:28 +0200

    Add toast when user is disconnected

--- client/src/store/modules/common.js
+++ client/src/store/modules/common.js
@@ -1,5 +1,6 @@
 import * as types from '../mutation-types'
 import i18n from '../../i18n'
+import izitoast from 'izitoast'
 
 const state = {
   connected: false,
@@ -16,14 +17,28 @@ const mutations = {
   [types.SOCKET_CONNECT] (state) {
     state.connected = true
   },
-  [types.SOCKET_DISCONNECT] (state) {
+  [types.DISCONNECT] (state) {
     state.connected = false
+  },
+  [types.SOCKET_PONG] (state, ms) {
   }
 }
 
 const actions = {
   changeLocale ({ commit }, locale) {
     commit(types.SET_LOCALE, locale)
+  },
+  socket_disconnect ({ commit }) {
+    commit(types.DISCONNECT)
+    izitoast.show({
+      title: i18n.t('toast:disconnect:title'),
+      message: i18n.t('toast:disconnect:text'),
+      color: 'red',
+      timeout: 0
+    })
+  },
+  socket_reconnect ({ commit, state }) {
+    console.log('reconnect?')
   }
 }
 
