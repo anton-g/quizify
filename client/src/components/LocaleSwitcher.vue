e500598ceccb9a2f9881344b315fd651941d6b6d
Anton Gunnarsson <gson.anton@hotmail.com>
2018-08-22 19:43:13 +0200

    Add missing sr label to locale switcher

--- client/src/components/LocaleSwitcher.vue
+++ client/src/components/LocaleSwitcher.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   .locale-switcher
+    label.is-sr-only(for="select-locale-input") {{ $t('locale:select:sr') }}
     .field
       .control.has-icons-left
         .select
-          select(@change="updateLang", v-model="selectedLocale")
+          select(@change="updateLang", v-model="selectedLocale", id="select-locale-input")
             option(v-for="(lang, i) in langs", :key="`Lang${i}`", :value="lang.locale") {{ lang.name }}
         .icon.is-left
           FontAwesomeIcon(:icon="icon")
@@ -47,3 +48,14 @@ export default {
 
 <style lang="scss">
 </style>
+
+<i18n>
+{
+  "en": {
+    "locale:select:sr": "Select language"
+  },
+  "sv": {
+    "locale:select:sr": "Välj språk"
+  }
+}
+</i18n>
